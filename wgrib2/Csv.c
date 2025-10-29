5d157fcd256cdfb39db50234d7429bb593d1b6f4
Tuan Tran Bui Anh <81059881+tuantba@users.noreply.github.com>
2025-10-29 23:24:30 +0700

    updated

--- wgrib2/Csv.c
+++ wgrib2/Csv.c
@@ -39,6 +39,9 @@ extern double *lon;
 /** Flag to indicate lat-lon grid processing. */
 extern int latlon;
 
+/** Number of grid points in x and y directions (assumed available in wgrib2). */
+extern int nx, ny;
+
 /*
  * HEADER:100:csv:output:1:make comma separated file, X=file (WxText enabled)
  */
@@ -49,15 +52,16 @@ extern int latlon;
  * with a different output format. 
  * 
  * The format is: 
- *      "time0","time1","field","level",longitude,latitude,grid-value
+ *      When WxNum > 0: i,j,"WxLabel(data[j])"
+ *      Otherwise: "time0","time1","field","level",longitude,latitude,grid-value
  * 
  * For an analysis that is valid at a specific time, time0 and time1 will have the same value. 
  * For a temporal average or accumulation of analyses, time0 will be the start and time1 will 
  * be end of the average or accumulation.
  *
  * For a forecast, time0 will be the start of the forecast (i.e, date of the initial conditions). 
- * For a forecast for a specific time, then time1 will be time that the forecast is valid. For a 
- * forecast of a temporal average or accumulation, time1 wll be the end of the average or accumulation.
+ * For a forecast for a specific time, then time1 will be the time that the forecast is valid. For a 
+ * forecast of a temporal average or accumulation, time1 will be the end of the average or accumulation.
  *
  * In grib speak, time0 is the reference time and time1 is the verification time.
  * 
@@ -87,10 +91,10 @@ extern int latlon;
  * 
  * @note The size of a CSV file can be overwhelming. One technique is to only generate the CSV for 
  * variables that you need. This can be done with the -match option. Even with the -match option, 
- * the high-resolution models can stil generate huge files. The next technique is to undefine the
+ * the high-resolution models can still generate huge files. The next technique is to undefine the
  * grid points that you don't want. For example, you are only interested in Hawaii (its 0C outside 
- * as I write this). Then you can use the the -undefine option to set the grid points outside of 
- * the Hawaii domain to undefined. Since -csv_long doesn't print undefined grid points, the CSV 
+ * as I write this). Then you can use the -undefine option to set the grid points outside of the 
+ * Hawaii domain to undefined. Since -csv_long doesn't print undefined grid points, the CSV 
  * files is much smaller. 
  * 
  * @note Warning 1: The options -csv, -csv_long, -spread and -text do not support memory files. You 
@@ -98,7 +102,7 @@ extern int latlon;
  * large and shouldn't be saved in memory. 
  * 
  * @note Warning 2: It may be tempting to take a grib file, convert it into a CSV file and then deal 
- * with the CSV file. After all, everybody can read a CSV file. Sure there is a litte overhead of reading 
+ * with the CSV file. After all, everybody can read a CSV file. Sure there is a little overhead of reading 
  * a CSV file but who cares. Suppose you want to read some GFS forecasts files (20 forecast times, 5 days 
  * every 6 hours) at 0.25 x 0.25 degree global resolution. Your CSV file is going to be about 720 GBs. Suppose 
  * that our hard drive can write/read at 70 MB/s. Then we are talking about 3 hours to write the CSV file and 3 
@@ -117,15 +121,15 @@ int f_csv(ARG1) {
     char name[100], desc[100], unit[100];
     FILE *out;
 
-    unsigned int j;
-    char vt[20],rt[20];
+    unsigned int j, i, k;
+    char vt[20], rt[20];
     int year, month, day, hour, minute, second;
 
     /* initialization phase */
 
     if (mode == -1) {
         WxText = decode = latlon = 1;
-        if ((*local = (void *) ffopen(arg1,file_append ? "a" : "w")) == NULL)
+        if ((*local = (void *) ffopen(arg1, file_append ? "a" : "w")) == NULL)
             fatal_error("csv could not open file %s", arg1);  
         return 0;
     }
@@ -140,49 +144,46 @@ int f_csv(ARG1) {
     /* processing phase */
 
     if (lat == NULL || lon == NULL) {
-        fprintf(stderr,"csv: latitude/longitude not defined, record skipped\n");
+        fprintf(stderr, "csv: latitude/longitude not defined, record skipped\n");
         return 0;
     }
 
     out = (FILE *) *local;
 
-    /*Collect runtime and validtime into vt and rt*/
+    /* Collect runtime and validtime into vt and rt */
 
     reftime(sec, &year, &month, &day, &hour, &minute, &second);
-    sprintf(rt, "%4.4d-%2.2d-%2.2d %2.2d:%2.2d:%2.2d", year,month,day,hour,minute,second);
+    sprintf(rt, "%4.4d-%2.2d-%2.2d %2.2d:%2.2d:%2.2d", year, month, day, hour, minute, second);
 
     vt[0] = 0;
     if (verftime(sec, &year, &month, &day, &hour, &minute, &second) == 0) {
-        sprintf(vt,"%4.4d-%2.2d-%2.2d %2.2d:%2.2d:%2.2d", year,month,day,hour,minute,second);
+        sprintf(vt, "%4.4d-%2.2d-%2.2d %2.2d:%2.2d:%2.2d", year, month, day, hour, minute, second);
     }
 
-    /*Get levels, parameter name, description and unit*/
+    /* Get levels, parameter name, description and unit */
 
     *new_inv_out = 0;
-    f_lev(call_ARG0(new_inv_out,NULL));
+    f_lev(call_ARG0(new_inv_out, NULL));
 
-    if (strcmp(new_inv_out, "reserved")==0) return 0;
-//    getName(sec, mode, NULL, name, desc, unit);
+    if (strcmp(new_inv_out, "reserved") == 0) return 0;
     getExtName(sec, mode, NULL, name, desc, unit);
-//	fprintf(stderr,"Start processing of %s at %s\n", name, new_inv_out);
-//	fprintf(stderr,"Gridpoints in data: %d\n", ndata);
-//	fprintf(stderr,"Description: %s, Unit %s\n", desc,unit);
 
-     /* Lage if-setning rundt hele som sjekker om alt eller deler skal ut*/
+    /* Write data to CSV file */
 
     if (WxNum > 0) {
         for (j = 0; j < ndata; j++) {
-            if (!UNDEFINED_VAL(data[j])) {
-                fprintf(out,"\"%s\",\"%s\",\"%s\",\"%s\",%g,%g,\"%s\"\n",rt,vt,name,
-                new_inv_out,lon[j] > 180.0 ?  lon[j]-360.0 : lon[j],lat[j],WxLabel(data[j]));
+            if (!UNDEFINED_VAL(data[j]) && data[j] > 0) {
+                i = j / nx;  /* row index */
+                k = j % nx;  /* column index */
+                fprintf(out, "%u,%u,\"%s\"\n", i, k, WxLabel(data[j]));
             }
         }
     }
     else {
         for (j = 0; j < ndata; j++) {
             if (!UNDEFINED_VAL(data[j])) {
-                fprintf(out, "\"%s\",\"%s\",\"%s\",\"%s\",%g,%g,%lg\n",rt,vt,name,
-                    new_inv_out,lon[j] > 180.0 ?  lon[j]-360.0 : lon[j],lat[j],data[j]);
+                fprintf(out, "\"%s\",\"%s\",\"%s\",\"%s\",%g,%g,%lg\n", rt, vt, name,
+                    new_inv_out, lon[j] > 180.0 ? lon[j] - 360.0 : lon[j], lat[j], data[j]);
             }
         }
     }
